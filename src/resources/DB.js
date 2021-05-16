834e559db6418bc0ad73736080cbfbaa273a3c75
Mahti <mahtishavaev@gmail.com>
2021-05-16 09:03:41 +0300

    refactor: delete mock data

--- src/resources/DB.js
+++ src/resources/DB.js
@@ -1,14 +1,7 @@
-const Board = require('./boards/board.model');
-const User = require('./users/user.model');
-
 const DB = {
   users: [],
   boards: [],
   tasks: [],
 };
-
-DB.users.push(new User(), new User(), new User());
-
-DB.boards.push(new Board({ title: 'Board 1' }));
 
 module.exports = DB;
