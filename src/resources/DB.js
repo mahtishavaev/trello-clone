a4eb1cd0ff2af5fe2abb983aebcd9abfb3157605
Mahti <mahtishavaev@gmail.com>
2021-05-16 08:52:18 +0300

    feat: add tasks endpoint

--- src/resources/DB.js
+++ src/resources/DB.js
@@ -4,6 +4,7 @@ const User = require('./users/user.model');
 const DB = {
   users: [],
   boards: [],
+  tasks: [],
 };
 
 DB.users.push(new User(), new User(), new User());
---
+++ src/resources/tasks/task.router.js
@@ -0,0 +1,38 @@
+const router = require('express').Router({ mergeParams: true });
+const tasksService = require('./task.service');
+
+router.route('/').get(async (req, res) => {
+  const { boardId } = req.params;
+  const tasks = await tasksService.getAll(boardId);
+  res.status(200).json(tasks);
+});
+
+router.route('/:id').get(async (req, res) => {
+  const { id } = req.params;
+  const task = await tasksService.getById(id);
+  if (task) {
+    res.status(200).json(task);
+  } else {
+    res.status(404).json({ message: "Task doesn't exist " });
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  const { boardId } = req.params;
+  const task = await tasksService.create(boardId, req.body);
+  res.status(201).json(task);
+});
+
+router.route('/:id').put(async (req, res) => {
+  const { id } = req.params;
+  const task = await tasksService.update(id, req.body);
+  res.status(200).json(task);
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const { id } = req.params;
+  await tasksService.remove(id);
+  res.status(204).json({ message: 'Task has been deleted' });
+});
+
+module.exports = router;
